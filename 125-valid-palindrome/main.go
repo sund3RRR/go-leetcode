535d9860543ce4d41969760e2fc0f68473388c6e
sund3RRR <evenquantity@gmail.com>
2026-05-23 09:47:52 +0300

    feat: add 125-valid-palindrome solution

---
+++ 125-valid-palindrome/main.go
@@ -0,0 +1,35 @@
+// 125. Valid Palindrome
+//
+// https://leetcode.com/problems/valid-palindrome/description/
+//
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+// and removing all non-alphanumeric characters, it reads the same forward and backward.
+// Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+func isPalindrome(s string) bool {
+	s = clearString(s)
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func clearString(s string) string {
+	var b strings.Builder
+	b.Grow(len(s))
+	for _, r := range s {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			b.WriteRune(unicode.ToLower(r))
+		}
+	}
+	return b.String()
+}
