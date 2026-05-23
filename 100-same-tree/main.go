c8d35e2908c8d9b7987f2476e8e15c7705514620
sund3RRR <evenquantity@gmail.com>
2026-05-23 09:42:03 +0300

    feat: add 100-same-tree solution

---
+++ 100-same-tree/main.go
@@ -0,0 +1,29 @@
+// 100. Same Tree
+//
+// https://leetcode.com/problems/same-tree/description/
+//
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil && q != nil {
+		return false
+	}
+
+	if p != nil && q == nil {
+		return false
+	}
+
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
